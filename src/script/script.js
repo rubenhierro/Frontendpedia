7b5ecf3a7de70477b307640cbb27b8a35d71ded2
Rubèn Hierro <rubenhierro@gmail.com>
2020-04-05 12:12:54 +0200

    Script index.html

---
+++ src/script/script.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('gift');
+gifts = [
+    'Macboo Pro',
+    'Iphone 15 Max',
+    'Viaje a Marbella',
+    'BMW Serie 2 Gran Coupé',
+    'Apple Watch serie 5',
+    'Curso de FrontEnd para Dummies'
+];
+const getGift = (gifts) => {
+    max = gifts.length;
+    randomNumber = Math.floor(Math.random() * Math.floor(max));
+    return gifts[randomNumber];
+}
+const openGift = () => alert(`Enhorabuena, te acaba de tocar un ${getGift(gifts)}`);
+
+button.addEventListener('click', openGift);
